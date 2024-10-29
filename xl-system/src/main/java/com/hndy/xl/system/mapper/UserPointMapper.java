9fdd4b55c0c5357c6922c919b4d85fd9387bd600
TWTW <2952560241@qq.com>
2024-10-29 23:52:45 +0800

    完成他的用户表和我自定义的用户表数据的CRUD

---
+++ xl-system/src/main/java/com/hndy/xl/system/mapper/UserPointMapper.java
@@ -0,0 +1,19 @@
+package com.hndy.xl.system.mapper;
+
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+public interface UserPointMapper
+{
+    @Insert("INSERT INTO t_user_points(dept_id, user_id, update_time) VALUES(#{deptId}, #{userId}, sysdate())")
+    void addPoints(@Param("deptId") Long deptId, @Param("userId") Long userId);
+
+    @Update("UPDATE t_user_points SET dept_id = #{deptId} where user_id = #{userId}")
+    void updatePoints(@Param("deptId")Long deptId,@Param("userId")  Long userId);
+
+    @Delete("DELETE FROM t_user_points where user_id = #{userId}")
+    void deletePoints(@Param("userId") Long userId);
+}
