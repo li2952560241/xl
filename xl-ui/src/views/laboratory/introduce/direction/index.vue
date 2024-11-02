73997aff4aac303841b90b9976a9a51d02d53adf
TWTW <2952560241@qq.com>
2024-11-02 23:35:46 +0800

    方向介绍文件word显示成功

---
+++ xl-ui/src/views/laboratory/introduce/direction/index.vue
@@ -0,0 +1,7 @@
+
+<template>
+<div>
+  <h1>编辑各个方向的介绍</h1>
+</div>
+</template>
+
---
+++ xl-ui/src/views/laboratory/introduce/show/crypto/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div v-html="wordContent"></div>
+  </div>
+</template>
+
+<script>
+import mammoth from "mammoth";
+
+export default {
+  data() {
+    return {
+      wordContent: '',
+    };
+  },
+
+  created() {
+    // 加载本地 Word 文件
+    this.loadWordFile();
+  },
+
+  methods: {
+    loadWordFile() {
+      // 直接从 public 目录加载 Word 文件
+      fetch('/assets/crypto_introduce.docx') // 确保路径正确
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('网络响应失败');
+          }
+          return response.arrayBuffer();
+        })
+        .then(arrayBuffer => {
+          return mammoth.convertToHtml({ arrayBuffer: arrayBuffer });
+        })
+        .then(result => {
+          this.wordContent = result.value; // Word 内容
+        })
+        .catch(err => {
+          console.error('加载 Word 文件失败', err);
+        });
+    }
+  }
+}
+</script>
